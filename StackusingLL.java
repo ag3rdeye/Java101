f293baca0048d15c1c939194eb8f255cca33aa59
Arzun Giri <102740402+ag3rdeye@users.noreply.github.com>
2022-04-02 12:25:45 +0545

    Add files via upload

---
+++ StackusingLL.java
@@ -0,0 +1,84 @@
+/** 
+    @author Arzun Giri
+ */
+import java.util.Scanner;
+public class StackusingLL {
+   private Node top;
+   private int size; 
+   class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public void push(int data){
+        Node newNode = new Node(data);
+            newNode.next = top;
+            top = newNode;
+            size++;
+        }
+    public  void pop(){ 
+        if(!isEmpty()){
+            System.out.println("Item popped: "+top.data);
+            top=top.next;
+            size--;
+        }
+        else{
+            System.out.println("Error Popping: Stack is Empty.");
+        }
+    }
+    void display(){
+       if(!isEmpty()){
+        Node temp = top;
+        while(temp!=null){
+            System.out.println(temp.data);
+            System.out.println("----");
+            temp=temp.next;
+        }
+       }
+       else{
+           System.out.println("Error: Stack is Empty.");
+       }
+    }
+    public boolean isEmpty(){
+        if(size==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+      // return (size==0)? true: false;
+    }
+    public static void main(String[] args) {
+        StackusingLL st = new StackusingLL();
+        Scanner in = new Scanner(System.in);
+        int choice;
+        System.out.println("1: To Push");
+        System.out.println("2: To Pop");
+        System.out.println("3: To Display");
+        System.out.println("0: To Exit");
+        do{
+            choice = in.nextInt();
+            switch(choice){
+                case 1:
+                System.out.println("Enter a number:");
+                    st.push(in.nextInt());
+                    break;
+                case 2:
+                    st.pop();
+                    break;
+                case 3:
+                    st.display();
+                    break;
+                case 0:
+                    return;
+                default:
+                    System.out.println("Error: Invalid Choice.");
+            }
+        }while(choice!=0);
+    }
+    
+}
