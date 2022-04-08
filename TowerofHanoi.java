bc64d48c3688c25b2e6288ca0f1ea1343420d584
Arzun Giri <102740402+ag3rdeye@users.noreply.github.com>
2022-04-08 23:39:35 +0545

    Add files via upload

---
+++ TowerofHanoi.java
@@ -0,0 +1,17 @@
+/**
+ * @author Arzun Giri
+ */
+public class TowerofHanoi {
+    public void TOH(char a,char b, char c, int n){
+       if(n>0){
+        TOH(a, c, b, n-1);
+        System.out.println("Move "+n+" from " +a+" to "+c+" using "+b);
+        TOH(b, a, c, n-1);
+       }
+    }
+    public static void main(String[] args) {
+        TowerofHanoi toh = new TowerofHanoi();
+        toh.TOH('a', 'b', 'c', 4);
+    }
+    
+}
